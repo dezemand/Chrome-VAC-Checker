31146450bb384d456251649a2f3fcbd80992a9f7
Maarten <maarten@v-ittersum.nl>
2015-04-29 21:33:33 +0200

    Fixed mess and added caching with chrome.storage.local

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
 var strangers = [].slice.call(document.querySelectorAll('.friendCheckbox2'));
 var friends = [].slice.call(document.querySelectorAll('.friendCheckbox'));
+var total = strangers.length + friends.length;
+var people = [];
+var query = [];
 
 strangers.forEach(function(stranger) {
   var id = stranger.outerHTML.match('friends\\[(.*?)\\]')[1];
-  getVACBan(id, stranger.parentNode.parentNode, setMessage);
+  addPeople({id: id, html: stranger.parentNode.parentNode});
 });
 
 friends.forEach(function(friend) {
   var url = friend.parentNode.children[1].getAttribute('onclick').split("top.location.href='")[1].split("'")[0];
-  if(url.indexOf("/profile/") === -1 && (url.indexOf("/id/") !== -1)) {
-    resolveVanityURL(url.split("/id/")[1], function(err, steamid) {
-      if(err) {alert(err); return;}
-      getVACBan(steamid, friend.parentNode, setMessage);
-    });
-  } else {
-    getVACBan(url.split("/profile/")[1], friend.parentNode, setMessage);
-  }
+  if(url.indexOf("/profile/") === -1 && (url.indexOf("/id/") !== -1))
+    resolveVanityURL(url.split("/id/")[1], function(err, steamid) {addPeople({id: steamid, html: friend.parentNode});});
+  else
+    addPeople({id: url.split("/profile/")[1], html: friend.parentNode});
 });
 
-function getVACBan(steamID, div, Callback) {
+function addPeople(obj) {
+  people.push(obj);
+  if(people.length == total) {
+    for(i=0; i<people.length; i++) {
+      getCache(people[i], function(err, obj, vac) {
+        if(vac && (vac.expire > (new Date().getTime()))) {
+          setMessage(vac, obj.html);
+        } else {
+          getAPIVAC(obj, function(data, person) {
+            setCache(person, data, function() {});
+            setMessage(data, person.html);
+          });
+        }
+      });
+    }
+  }
+}
+
+function getAPIVAC(person, Callback) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if(xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
-      return Callback(JSON.parse(xmlhttp.responseText).players[0], div);
+      return Callback(JSON.parse(xmlhttp.responseText).players[0], person);
     }
   };
-  xmlhttp.open('GET', 'https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=F6F90A461E30D38AB4AE8AADB5AD8658&steamids='+steamID, true);
+  xmlhttp.open('GET', 'https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=F6F90A461E30D38AB4AE8AADB5AD8658&steamids='+person.id, true);
   xmlhttp.send();
 }
 
@@ -51,3 +68,20 @@ function resolveVanityURL(vanityurl, Callback) {
   xmlhttp.open('GET', "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=F6F90A461E30D38AB4AE8AADB5AD8658&vanityurl=" + vanityurl, true);
   xmlhttp.send();
 }
+
+function getCache(person, Callback) {
+  chrome.storage.local.get("cache_"+person.id, function(obj, items) {
+    if(Object.keys(obj).length == 0) return Callback(null, person, null);
+    return Callback(null, person, obj["cache_"+person.id]);
+  });
+}
+
+function setCache(person, data, Callback) {
+  var obj = {};
+  obj["cache_"+person.id] = {expire: (new Date().getTime() + 1000*60*60*24), NumberOfVACBans: data.NumberOfVACBans, DaysSinceLastBan: data.DaysSinceLastBan};
+  chrome.storage.local.set(obj, function() {
+    return Callback();
+  });
+}
+
+chrome.storage.local.get("cache_76561198126170311", function(obj, items) {console.log(obj["cache_76561198126170311"], items);});
